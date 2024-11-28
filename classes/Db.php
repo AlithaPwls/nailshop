d1f780926af5a4b8ed146c375e0ab98465a8529a
alithapwls <r1011638@student.thomasmore.be>
2024-11-28 10:24:42 +0100

    Database-verbinding aangepast voor Railway

--- classes/Db.php
+++ classes/Db.php
@@ -6,8 +6,17 @@ class Db {
     public static function getConnection(){
         //aanroepen met Db::getConnection();
         if( self::$conn == null){
-            self::$conn = new PDO ('mysql:host=autorack.proxy.rlwy.net;dbname=railway', 'root', 'mqgnaGyxaOteBJWUlDOjnuEDSJmuuGNQ');
-            return self::$conn;
+            $db_url = getenv('DATABASE_URL'); // Vraag het geheim briefje van Railway
+            $db_parts = parse_url($db_url);   // Splits het in stukjes
+            
+            $host = $db_parts['host'];
+            $port = $db_parts['port'];
+            $user = $db_parts['user'];
+            $pass = $db_parts['pass'];
+            $dbname = ltrim($db_parts['path'], '/');
+            
+            self::$conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+                        return self::$conn;
         }
         else {
             return self::$conn;
