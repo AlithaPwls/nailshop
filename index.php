a8ceb122ff1a784bc489f0e1920a310f9c4d6f38
alithapwls <r1011638@student.thomasmore.be>
2024-11-28 01:15:10 +0100

    db connection

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
-/*
+
 $pathToSSL = __DIR__ . "/private/ssl/ssl.txt";
 $options = array(
     PDO::MYSQL_ATTR_SSL_CA => $pathToSSL
 );
 
-$host = 'pinkgellacmysql12.mysql.database.azure.com';
+$host = 'nailhopdb.mysql.database.azure.com';
 $db = 'nailshop';
 $user = 'pinkgellac';
 $pass = 'Minions2001!';
 $db = new PDO("mysql:host=$host;dbname=$db", $user, $pass, $options);
-*/
+
+var_dump($users->fetchAll());
 
 
 session_start();  // Zorg ervoor dat de sessie eerst wordt gestart
