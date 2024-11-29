755cd3d920b2f04d5326594361b8fecff26a6905
alithapwls <r1011638@student.thomasmore.be>
2024-11-29 17:11:51 +0100

    certificaat

--- classes/Db.php
+++ classes/Db.php
@@ -1,21 +1,28 @@
 <?php 
 
-    class Db {
-        private static $conn = null;
+class Db {
+    private static $conn = null;
 
-        public static function getConnection(){
-            //aanroepen met Db::getConnection();
-            if( self::$conn == null){
-                self::$conn = new PDO ('mysql:host=localhost;dbname=shop', 'root', '');
-                return self::$conn;
-            }
-            else {
-                return self::$conn;
+    public static function getConnection(){
+        // Aanroepen met Db::getConnection();
+        if (self::$conn == null) {
+            $pathToSSL = __DIR__ . '/CA.pem';
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => $pathToSSL,
+            );
+
+            $host = 'fvsfdvfdvfdbvfbvf';
+            $db = 'shop';
+            $user = 'frgfvg';
+            $pass = 'Minions2001!';
+            
+            try {
+                self::$conn = new PDO("mysql:host=$host;port=3306;dbname=$db", $user, $pass, $options);
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Connection failed: " . $e->getMessage());
             }
         }
+        return self::$conn;
     }
-
-
-
-
- 
+}
