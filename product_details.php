88e7232b56d9b240e8cde5209fe6af44d32f7f2e
alithapwls <r1011638@student.thomasmore.be>
2024-12-02 17:01:50 +0100

    d

--- product_details.php
+++ product_details.php
@@ -2,7 +2,7 @@
 session_start();
 
 include_once (__DIR__ . "/classes/products.php");
-include_once (__DIR__ . "/classes/Review.php");
+include_once (__DIR__ . "/classes/review.php");
 include_once (__DIR__ . "/classes/Order.php");
 
 // Controleer of de gebruiker is ingelogd
