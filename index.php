b67f3516e504e933732ed3bd3861dc434d21bd9b
alithapwls <r1011638@student.thomasmore.be>
2024-12-02 18:44:26 +0100

    deded

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ $colorgroup = $_GET['color_group'] ?? 'all';
                 ): ?>
                     <a href="product_details.php?id=<?= $product['id']; ?>">
                         <article>
-                            <img src="/nailshop/images<?= $product['image_url']; ?>" alt="Product image">
+                            <img src="<?= $product['image_url']; ?>" alt="Product image">
                             <h2><?= $product['color_name'] . " - " . $product['color_number']; ?></h2>
                             <h3>€<?= number_format($product['price'], 2); ?></h3>
                             <button class="add" data-product-id="<?= $product['id']; ?>">Add to cart</button>
@@ -116,7 +116,6 @@ $colorgroup = $_GET['color_group'] ?? 'all';
     }
 </script>
 <script>
-?>
 document.querySelectorAll('.add').forEach(button => {
     button.addEventListener('click', function (event) {
         event.preventDefault();
