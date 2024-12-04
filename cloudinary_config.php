7faebf298c8b5f864cfaba46ea2dff15ddcc623e
alithapwls <r1011638@student.thomasmore.be>
2024-12-04 13:46:27 +0100

    Integrate Cloudinary for image uploads

---
+++ cloudinary_config.php
@@ -0,0 +1,9 @@
+<?php
+require 'vendor/autoload.php';
+
+\Cloudinary::config(array( 
+  "cloud_name" => "nailshop", 
+  "api_key" => "795931936958539", 
+  "api_secret" => "nkAVSDYQlvEX3bQMNCzHfTDf46c" 
+));
+?>
